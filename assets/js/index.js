10cd6bff49d38aa6d00f7efe8a0faebfda0e2f6d
Kavin Valli <kavinvalli@gmail.com>
2021-09-21 18:44:09 +0530

    feat: Make navbar and footer reusable

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+async function fetchHtml(url) {
+  return await (await fetch(url)).text();
+}
+
+async function loadComponentAsHtml(id, htmlFile) {
+  const targetDiv = document.getElementById(id);
+  targetDiv.innerHTML = await fetchHtml(`components/${htmlFile}.html`);
+}
+
+loadComponentAsHtml("footer", "footer");
+console.log("hi");
+
+const links = [
+  {
+    name: "Home",
+    html: "index.html",
+  },
+  {
+    name: "About us",
+    html: "about.html",
+  },
+  {
+    name: "Team",
+    html: "team.html",
+  },
+  {
+    name: "Blog",
+    html: "blog.html",
+  },
+  {
+    name: "Contact",
+    html: "contact.html",
+  },
+];
+document.getElementById("navbar").innerHTML =
+  `<ul>` +
+  links.map(
+    (link) =>
+      `
+    <li><a href="${link.html}" ${
+        location.pathname.includes(link.html) && 'class="active"'
+      }>${link.name}</a></li>
+  `
+  ) +
+  `</ul>`;
